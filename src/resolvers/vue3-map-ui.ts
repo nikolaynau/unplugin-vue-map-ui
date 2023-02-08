92c86346eb99b8d821892edd57a2b33f059307b8
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2023-02-08 18:11:34 +0300

    feat: add tests

--- src/resolvers/vue3-map-ui.ts
+++ src/resolvers/vue3-map-ui.ts
@@ -4,7 +4,7 @@ export function Vue3MapUiResolver(): ComponentResolver {
   return {
     type: 'component',
     resolve: name => {
-      if (name.match(/^VMap[A-Z]/)) {
+      if (name.match(/^VMap[A-Z]?/)) {
         return {
           name,
           from: 'vue3-map-ui'
---
+++ test/resolvers/vue3-map-ui.test.ts
@@ -0,0 +1,38 @@
+import { afterEach, describe, expect, it } from 'vitest';
+import { build } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import vue from '@vitejs/plugin-vue';
+import { deleteSync } from 'del';
+import Components from 'unplugin-vue-components/vite';
+import { Vue3MapUiResolver } from '../../src';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const appDir = path.resolve(__dirname, '../fixtures/app');
+const distDir = path.resolve(appDir, './dist');
+
+describe('Vue3 Map UI Resolver', () => {
+  afterEach(() => {
+    deleteSync(distDir);
+  });
+
+  it('components should be auto imported', async () => {
+    const result: any = await build({
+      root: appDir,
+      plugins: [
+        vue(),
+        Components({
+          resolvers: [Vue3MapUiResolver()]
+        })
+      ],
+      build: {
+        minify: false,
+        rollupOptions: {
+          external: ['vue', 'vue3-map-ui']
+        }
+      }
+    });
+
+    expect(result.output[0].code).toMatchSnapshot();
+  });
+});
